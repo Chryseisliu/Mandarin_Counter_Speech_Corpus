fe24341c38e8ecfa5796c0f5ba4a684544d1bbcb
michaelbennie <michaelbennie@ufl.edu>
2024-11-17 18:56:12 -0500

    rotated answers

--- advercialModel/LLMAnswerGenerator.py
+++ advercialModel/LLMAnswerGenerator.py
@@ -7,7 +7,7 @@ class LLMAnswerGenerator:
 
 
     def __init__(self):
-        self.TOKEN = "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"
+        self.TOKEN = "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"
         self.clients = {
             "Hermes": InferenceClient(
                 "NousResearch/Hermes-3-Llama-3.1-8B",
